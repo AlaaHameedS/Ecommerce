a017fe6ea88bf708a06e238500f56e522f3c9c2a
AlaaHameedS <alaahameedsahar1995@gmail.com>
2024-06-23 09:52:33 +0300

    End Controller Part

---
+++ Ecommerce/Controllers/CategoriesController.cs
@@ -0,0 +1,21 @@
+﻿using Ecommerce.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Ecommerce.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        private readonly EcommerceDbContext _context;
+
+        public CategoriesController(EcommerceDbContext context)
+        {
+            _context = context;
+        }
+        public async Task <IActionResult> Index()
+        {
+            var Resoponse = await _context.Categories.ToListAsync();
+            return View(Resoponse);
+        }
+    }
+}
---
+++ Ecommerce/Controllers/ProductsController.cs
@@ -0,0 +1,21 @@
+﻿using Ecommerce.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Ecommerce.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private readonly EcommerceDbContext _context;
+
+        public ProductsController(EcommerceDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var Response = await _context.Products.ToListAsync();
+            return View(Response);
+        }
+    }
+}
