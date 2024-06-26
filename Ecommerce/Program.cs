e3a45ad3ff2a3e38751fc826e900951edbcf4597
AlaaHameedS <alaahameedsahar1995@gmail.com>
2024-06-26 10:32:04 +0300

    End View part

--- Ecommerce/Program.cs
+++ Ecommerce/Program.cs
@@ -32,7 +32,7 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Products}/{action=Index}/{id?}");
 
 AppDbInitializer.Seed(app);
 
